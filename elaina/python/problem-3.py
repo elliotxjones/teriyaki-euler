3ee84bc13e6cd320b62960437fed10cab37e97b6
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2023-08-06 17:47:33 -0700

    fix: Problem 3 in python
    Fixes problem 3 solution in Python. I don't even know why it broke.

--- elaina/python/problem-3.py
+++ elaina/python/problem-3.py
@@ -4,20 +4,22 @@
 # What is the largest prime factor of the number 600851475143?
 
 def breakdown(n):
-    for i in range(1,n+1):
-        print(i)
+    for i in range(1,int(n)+1):
         if all([
             n % i == 0,     
-            n != 1,
+            i != 1,
+            n != i,
         ]):
             n = n/i
             return n
-                
+
 def main():
-    old_num = 600851475143
-    new_num = 0
-    while new_num != old_num:
-        new_num = breakdown(old_num)
-    print(new_num)
+    num = 600851475143
+    old_num = num
+    while num:
+        old_num = num
+        num = breakdown(old_num)
+    print(old_num)
+
 
 main()
